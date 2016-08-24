377a5af38deef6e457586daecfe5dfd3010a2678
ant.pu <puhongwu@live.com>
2016-08-24 13:00:11 +0800

    rm device info

--- src/android/TffConfig.java
+++ src/android/TffConfig.java
@@ -33,20 +33,7 @@ import android.provider.Settings;
 public class TffConfig extends CordovaPlugin {
     public static final String TAG = "TffConfig";
 
-    public static String platform;                            // Device OS
-    public static String uuid;                                // Device UUID
-
     private String channel = "";   //app发布渠道
-
-    private static final String ANDROID_PLATFORM = "Android";
-    private static final String AMAZON_PLATFORM = "amazon-fireos";
-    private static final String AMAZON_DEVICE = "Amazon";
-
-    /**
-     * Constructor.
-     */
-    public TffConfig() {
-    }
 
     /**
      * Sets the context of the Command. This can then be used to do things like
@@ -58,7 +45,6 @@ public class TffConfig extends CordovaPlugin {
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
         channel = webView.getPreferences().getString("TffConfig_Channel", "");
-        TffConfig.uuid = getUuid();
     }
 
     /**
@@ -73,106 +59,12 @@ public class TffConfig extends CordovaPlugin {
         if ("channel".equals(action)) {
             JSONObject r = new JSONObject();
             r.put("channel", channel);
-            r.put("uuid", TffConfig.uuid);
-            r.put("version", this.getOSVersion());
-            r.put("platform", this.getPlatform());
-            r.put("model", this.getModel());
-            r.put("manufacturer", this.getManufacturer());
-	        r.put("isVirtual", this.isVirtual());
-            r.put("serial", this.getSerialNumber());
             callbackContext.success(r);
         }
         else {
             return false;
         }
         return true;
-    }
-
-    //--------------------------------------------------------------------------
-    // LOCAL METHODS
-    //--------------------------------------------------------------------------
-
-    /**
-     * Get the OS name.
-     *
-     * @return
-     */
-    public String getPlatform() {
-        String platform;
-        if (isAmazonDevice()) {
-            platform = AMAZON_PLATFORM;
-        } else {
-            platform = ANDROID_PLATFORM;
-        }
-        return platform;
-    }
-
-    /**
-     * Get the device's Universally Unique Identifier (UUID).
-     *
-     * @return
-     */
-    public String getUuid() {
-        String uuid = Settings.Secure.getString(this.cordova.getActivity().getContentResolver(), android.provider.Settings.Secure.ANDROID_ID);
-        return uuid;
-    }
-
-    public String getModel() {
-        String model = android.os.Build.MODEL;
-        return model;
-    }
-
-    public String getProductName() {
-        String productname = android.os.Build.PRODUCT;
-        return productname;
-    }
-
-    public String getManufacturer() {
-        String manufacturer = android.os.Build.MANUFACTURER;
-        return manufacturer;
-    }
-
-    public String getSerialNumber() {
-        String serial = android.os.Build.SERIAL;
-        return serial;
-    }
-
-    /**
-     * Get the OS version.
-     *
-     * @return
-     */
-    public String getOSVersion() {
-        String osversion = android.os.Build.VERSION.RELEASE;
-        return osversion;
-    }
-
-    public String getSDKVersion() {
-        @SuppressWarnings("deprecation")
-        String sdkversion = android.os.Build.VERSION.SDK;
-        return sdkversion;
-    }
-
-    public String getTimeZoneID() {
-        TimeZone tz = TimeZone.getDefault();
-        return (tz.getID());
-    }
-
-    /**
-     * Function to check if the device is manufactured by Amazon
-     *
-     * @return
-     */
-    public boolean isAmazonDevice() {
-        if (android.os.Build.MANUFACTURER.equals(AMAZON_DEVICE)) {
-            return true;
-        }
-        return false;
-    }
-
-    public boolean isVirtual() {
-	return android.os.Build.FINGERPRINT.contains("generic") ||
-	    android.os.Build.PRODUCT.contains("sdk");
     }
 
 }
